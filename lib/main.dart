3cfd660c7d7ae4a44958fcaa12f64b85213dff95
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-05-30 16:04:40 +0545

    Added streamProvider in main.dart and user stream to wrapper

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:first_flutter_brew/models/user.dart';
 import 'package:first_flutter_brew/screens/wrapper.dart';
+import 'package:first_flutter_brew/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Wrapper(),
+    return StreamProvider<User>.value(
+      value: AuthService().user, // accessing user stream from auth service
+      child: MaterialApp(
+        home: Wrapper(),
+      ),
     );
   }
 }
--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,10 +1,16 @@
+import 'package:first_flutter_brew/models/user.dart';
 import 'package:first_flutter_brew/screens/authenticate/authenticate.dart';
-// import 'package:first_flutter_brew/screens/home/home.dart';
+import 'package:first_flutter_brew/screens/home/home.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class Wrapper extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final user =
+        Provider.of<User>(context); // what type of data to fetched from context
+    print(user);
+    print("here");
     // return either Home or Authenticate widget
     return Authenticate();
   }
