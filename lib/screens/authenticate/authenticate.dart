9a28fe183b3aff7d5681bfbde9f11e1cf4f81f5a
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-05-30 19:50:07 +0545

    Added signin or register logic

--- lib/screens/authenticate/authenticate.dart
+++ lib/screens/authenticate/authenticate.dart
@@ -1,3 +1,4 @@
+import 'package:first_flutter_brew/screens/authenticate/register.dart';
 import 'package:first_flutter_brew/screens/authenticate/sign_in.dart';
 import 'package:flutter/material.dart';
 
@@ -7,11 +8,16 @@ class Authenticate extends StatefulWidget {
 }
 
 class _AuthenticateState extends State<Authenticate> {
-  
+  bool showSignIn = true;
+
+  void toggleView() {
+    setState(() {
+      showSignIn = !showSignIn;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SignIn(),
-    );
+    return showSignIn ? SignIn(toggleView: toggleView) : Register(toggleView: toggleView);
   }
 }
