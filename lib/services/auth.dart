8c713786494cdcd654269ac1ae036244907fc2a4
Parajuli Kiran <kiranparajuli589@gmail.com>
2020-05-30 15:38:08 +0545

    Added user model for app and create user instance after signInAnonymosly

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,21 +1,27 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:first_flutter_brew/models/user.dart';
 
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
 
-  // signin Anonymously
+  // create user obj based on FirebaseUser
+  User _userFromFirebaseUser(FirebaseUser user) {
+    return user != null ? User(uid: user.uid): null;
+  }
+
+  // sign in Anonymously
   Future signInAnonymously() async {
     try {
       AuthResult result = await _auth.signInAnonymously();
       FirebaseUser user = result.user;
-      return user;
+      return _userFromFirebaseUser(user);
     } catch (e) {
         print(e.toString());
         return null;
       }
   }
 
-  // signin with email & password
+  // sign in with email & password
 
   //  register with email & password
 
